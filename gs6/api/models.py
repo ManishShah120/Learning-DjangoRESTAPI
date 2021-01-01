498be9253cdf29f74927a1b0d99a56fe9ac972f6
ManishShah120 <mkshah141@gmail.com>
2021-01-02 01:46:21 +0530

    GS6
    Learned about ModelSerialization

---
+++ gs6/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll = models.IntegerField()
+    city = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('id', )
