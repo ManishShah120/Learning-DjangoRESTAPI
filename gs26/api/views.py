eded6e419f2a2e57308e53ff98d3b211098cfec7
ManishShah120 <mkshah141@gmail.com>
2021-01-04 00:41:08 +0530

    GS26
    Learned to genrate, get API Toekn  with httpie and gettin more details be creating an auth.py file.

---
+++ gs26/api/views.py
@@ -0,0 +1,13 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class StudentModelViewset(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
---
+++ gs26/readme.md
@@ -0,0 +1,8 @@
+# Token Authentication for any client/user
+This authentication scheme uses a simple token-based HTTP Authentication scheme. Token authentication for client-server setups, such as native desktop and mobile clients.
+
+Till now learned:-
+- Using Admin Pnel Application
+- Using Django manage.py command
+- By exposing an API endpoint
+- Using Signals[gs27]
